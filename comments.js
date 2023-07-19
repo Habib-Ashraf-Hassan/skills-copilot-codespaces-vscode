c2405fff4d5eaca2f72fb65d39345581a04db696
Ashraf_Mohammed_Anil <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2023-07-19 08:05:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// create web server in js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.write('Hello World!');
+  res.end();
+});
+
+server.listen(8080);
